d52d04f8f72d0b5f9cc2e09a38bbcb6fa9771e3c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-09 17:10:45 -0300

    Requisito 07 OK

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const paletaDeCores = document.getElementById('color-palette');
+let classeSelected = document.createElement('class');
+let colorBlack = document.querySelectorAll('.color')[0];
+let colorRed = document.querySelectorAll('.color')[1];
+let colorBlue = document.querySelectorAll('.color')[2];
+let colorGreen = document.querySelectorAll('.color')[3];
+
 window.onload = function(){
-    const paletaDeCores = document.getElementById('color-palette');
-
-    let colorBlack = document.querySelectorAll('.color')[0];
-    let colorRed = document.querySelectorAll('.color')[1];
-    let colorBlue = document.querySelectorAll('.color')[2];
-    let colorGreen = document.querySelectorAll('.color')[3];
-
-    let classeSelected = document.createElement('class');
     colorBlack.className += ' selected'
     paletaDeCores.appendChild(classeSelected)
+}
 
+document.getElementsByClassName('color')[0].addEventListener('click',selecionaCor);
+document.getElementsByClassName('color')[1].addEventListener('click',selecionaCor);
+document.getElementsByClassName('color')[2].addEventListener('click',selecionaCor);
+document.getElementsByClassName('color')[3].addEventListener('click',selecionaCor);
+
+function selecionaCor(event){
+    const corSelecionada = document.querySelector('.selected');
+    corSelecionada.classList.remove('selected');
+    event.target.classList.add('selected');
 }
